572bbeca6b55ce34d3a549d7916b99fb45268ac3
Andrews Medina <andrewsmedina@gmail.com>
2016-06-26 15:37:31 -0300

    check vote

--- dreams/views.py
+++ dreams/views.py
@@ -4,10 +4,16 @@ from .forms import DreamsForm
 
 
 def dreams(request):
+    if request.session.get('voted'):
+        return redirect("/thankyou/")
+
     form = DreamsForm(request.POST or None)
+    
     if form.is_valid():
         form.save()
+        request.session['voted'] = True
         return redirect("/thankyou/")
+
     context = {"form": form}
     return render(request, "dreams/index.html", context)
 
